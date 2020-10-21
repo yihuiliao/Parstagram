b9f32a8cbfd78588fb35cf0cb2614c99293ed5f0
Yihui Liao <liao.yihui18@gmail.com>
2020-10-21 18:20:00 -0400

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Yihui Liao on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
