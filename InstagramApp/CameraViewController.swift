e5f0ee990f89a81fb427ecb07c71366635da0c9c
Yihui Liao <liao.yihui18@gmail.com>
2020-10-22 13:47:40 -0400

    added alamofireimage

---
+++ InstagramApp/CameraViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CameraViewController.swift
+//  InstagramApp
+//
+//  Created by Yihui Liao on 10/22/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[.editedImage] as! UIImage
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
