56565d224611559bdeb6a681e3f9430d5d351d44
Yihui Liao <liao.yihui18@gmail.com>
2020-10-21 21:58:04 -0400

    added parse/backend to allow users to sign up and sign in

--- InstagramApp/AppDelegate.swift
+++ InstagramApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "Qa43Y0vojMdsqxHk5j9DOy3875CmssAB8pR4Jp6f"
+                    $0.clientKey = "nZLskzP6LbMBn4xwXBeJNGWOT9Azol3MCESYZA1R"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
--- InstagramApp/LoginViewController.swift
+++ InstagramApp/LoginViewController.swift
@@ -6,9 +6,53 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password){(user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    
+    }
 
+    
+    @IBAction func signUp(_ sender: Any) {
+        
+        let user =  PFUser()
+        
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground(){(success, error) in
+            
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,7 +73,9 @@ class LoginViewController: UIViewController {
         
         gradientLayer.frame = self.view.bounds
         
-        gradientLayer.colors = [UIColor.purple.cgColor, UIColor.yellow.cgColor]
+        gradientLayer.colors = [UIColor.purple.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor]
+        
+        gradientLayer.locations = [0.0, 0.75, 0.98]
         
         self.view.layer.insertSublayer(gradientLayer, at: 0)
         
