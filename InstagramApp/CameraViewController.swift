fbccc00d0c0b129f00eb385efa20534edda84a78
Yihui Liao <liao.yihui18@gmail.com>
2020-10-22 14:25:06 -0400

    save picture to backend

--- InstagramApp/CameraViewController.swift
+++ InstagramApp/CameraViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 import AlamofireImage
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
@@ -24,6 +25,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func submitButton(_ sender: Any) {
+        
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground{(success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("success")
+            } else{
+                print("error")
+            }
+        }
         
     }
     
@@ -45,6 +65,13 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
     }
     /*
     // MARK: - Navigation
